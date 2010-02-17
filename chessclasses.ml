4ec10d6fd48148472448fc4c9d24fed27ba7be46
quentincormier <quentincormier@63ad2634-0ac7-11df-a48f-3fa99c96b54c>
2010-02-17 17:16:57 +0000

    minor bug spotted

--- chessclasses.ml
+++ chessclasses.ml
@@ -8,6 +8,22 @@ let (!!) = function
   | Black -> White
   | White -> Black
 ;;
+type score = PInf | N of int | MInf;;
+let (--) = function
+  | PInf -> MInf
+  | MInf -> PInf
+  | N n -> N (-n)
+;;
+let (>>) a b = match (a, b) with
+  | PInf, MInf
+  | MInf, PInf -> PInf
+  | PInf, PInf | MInf, MInf -> a
+  | MInf, N n 
+  | N n, MInf -> N n
+  | PInf, N n 
+  | N n, PInf -> PInf
+  | N n, N m -> N (max n m)
+;;
 type dep = 
     Castling of (int * int) * (int * int)
   | Enpassant of (int * int) * (int * int)
@@ -48,7 +64,7 @@ let get_option = function
    val mutable board = new board (8, 8) Empty
    val mutable turn = White
    val mutable king_w = (4,0)
-   val mutable king_b = (4,6)
+   val mutable king_b = (4,7)
    val mutable castling_w = (true, 0)
    val mutable castling_b = (true, 0)
 
@@ -61,7 +77,8 @@ let get_option = function
        let inline_piece = [|Rook; Knight; Bishop; Queen; King; Bishop; Knight; Rook|] in
 	 for i = 0 to 7 do b.(i).(0) <- Piece (inline_piece.(i), White) done;
 	 for i = 0 to 7 do b.(i).(7) <- Piece (inline_piece.(i), Black) done;
-	 board#fill b
+	 board#fill b;
+	 turn <- White
 
    method print = 
      let piece_al = 
@@ -75,7 +92,7 @@ let get_option = function
 	   (f Queen,  ('Q', stared));
 	   (f Rook,   ('R', stared));
 	   (f Bishop, ('B', stared));
-	   (f Knight, ('K', stared));
+	   (f Knight, ('N', stared));
 	   (f Pawn,   ('P', stared));
 	  ]
      in
@@ -85,7 +102,7 @@ let get_option = function
 
 
    method turn = turn
-   method private edit_turn = turn <- !!turn
+   method edit_turn = turn <- !!turn
 
    method  king color = if color = White then king_w else king_b
    method castling color = if color = White then castling_w else castling_b
@@ -136,6 +153,10 @@ let get_option = function
      board#rollback;
      self#edit_turn;
      try
+	 moves <- List.tl moves
+       with _ -> ();
+
+     try
        match List.hd moves with
 	 | Castling((a, b), (a', b')) ->
 	     self#edit_castling turn false false;
@@ -144,12 +165,9 @@ let get_option = function
 	     self#edit_castling turn true false;
 	     self#edit_king turn (a, b)
 	 | _ -> () 
-     with _ -> ();
-       try
-	 moves <- List.tl moves
-       with _ -> ()
+     with _ -> ()
 
-   method private mouvements_p (a, b) p castling  =
+   method  mouvements_p (a, b) p castling  =
      (* On récupère les mouvements de la piece *)
     let _, (l, (mult, _)) = List.find (fun (x, y) -> x = p) mouvements in
       (* Si c'est une piece noire, les mouvements sont inversés *)
@@ -219,17 +237,18 @@ let get_option = function
 		      )
    method check_move (a, b) (a', b') p_prom = 
     let r, mvt = self#is_check_move(a, b) (a', b') p_prom in
-    let pos_king = self#king turn in
       if r then begin
+	(* blanc *)
 	self#move_piece (get_option mvt);
 	self#edit_turn;
-	 let r' = self#is_check pos_king in
+	 let r' = self#is_check (self#king turn) in
 	self#edit_turn;
 	self#cancel;
 	  if r' then (false, None) else (true, mvt)
       end
       else (false, None)
 		  
+	(* Look if you are check *)
    method private is_check pos_king =
      self#edit_turn;
     let mvt_adv = self#get_all false false in
@@ -304,7 +323,8 @@ let get_option = function
 		   if board#get_interval ((=) Empty) (a, b) (a', b') mvt then (true, Some (Dep((a, b), (a', b'))))
 		   else 
 		    if p = King then 
-		       if self#check_castling (a, b) (a', b') then (true, Some (Castling((a, b), (a', b'))))
+		      if self#check_castling (a, b) (a', b') then 
+			(true, Some (Castling((a, b), (a', b'))))
 		       else  (false, None)
 		     else (false, None) 
 	   | _ -> (false, None)
@@ -318,6 +338,7 @@ let get_option = function
 	     if (pp <> Empty) && (get_color pp = turn) then
 	       ( 
 		 let l = self#mouvements_p (i, j) (get_piece pp) castling in
+		   	 print_int i; print_string " "; print_int j; print_newline();
 		   let nl = List.map (fun (c, prom) -> (if check_king then self#check_move else self#is_check_move) (i, j) c prom) l in 
 		 let nl = List.filter (fun (r, x) -> r = true) nl in  
 		   list := List.map (fun (_, dep) -> (get_option dep)) nl @ !list
@@ -331,21 +352,29 @@ let get_option = function
 
 
 
+ let play game = 
+   let l =  (game#get_moves true) in
+   let c = List.nth l (Random.int (List.length l)) in
 
+     game#move_piece c;
+   game#print 
+;;
 let game = new chess;;
-game#fac 5;;
-game#mouvements_p (4, 1) Pawn true ;;
 game#init;;
+game#king Black;;
+game#move_piece (Dep((4,0), (4,5)));;
+game#edit_king;;
+game#edit_turn;;
 let l = game#get_moves true;;
 game#moves;;
-List.length l;;
 game#print;;
-game#edit_turn;;
-game#is_valid_mouvement (3,0) (4,1) Queen;;
-game#move_piece (Dep((5,1), (5,2)));;
-game#castling Black;;
-game#castling White;;
-game#king White;;
-game#cancel;;
-game#edit_castling White;;
-game#castling Black;;
+play game;;
+game#print;;
+game#turn;;
+game#get_moves false;;
+let l = game#moves;;
+let l = [Dep ((5, 7), (2, 4)); Dep ((6, 0), (7, 2)); Dep ((0, 6), (0, 5));
+ Dep ((2, 1), (2, 3)); Dep ((4, 6), (4, 4)); Dep ((1, 1), (1, 3))];;
+List.iter game#move_piece l;;
+game#print;;
+game#mouvements_p (4,0) King false;;
